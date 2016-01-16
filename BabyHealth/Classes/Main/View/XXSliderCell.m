49e232ab6f59c364309902307d7c2e9f9aa2a7df
chenxu89 <675269196@qq.com>
2016-01-16 16:32:16 +0800

    add-添加了slider的一些特有属性

--- BabyHealth/Classes/Main/View/XXSliderCell.m
+++ BabyHealth/Classes/Main/View/XXSliderCell.m
@@ -18,6 +18,16 @@
 - (void)awakeFromNib
 {
     self.slider.delegate = self;
+    
+    // customize slider 1
+    self.slider.minimumValue = 100.0;// 最大值
+    self.slider.maximumValue = 255.0;// 最小值
+    self.slider.popUpViewCornerRadius = 5.0;// 弹窗圆角
+    [self.slider setMaxFractionDigitsDisplayed:0];// 小数位数
+    self.slider.popUpViewColor = [UIColor colorWithHue:0.55 saturation:0.8 brightness:0.9 alpha:0.7];// 弹窗颜色
+    self.slider.font = [UIFont fontWithName:@"GillSans-Bold" size:22];// 弹窗文字字体
+    self.slider.textColor = [UIColor colorWithHue:0.55 saturation:1.0 brightness:0.5 alpha:1];// 弹窗文字颜色
+    self.slider.popUpViewWidthPaddingFactor = 1.7;
 }
 
 - (void)sliderWillDisplayPopUpView:(ASValueTrackingSlider *)slider;
