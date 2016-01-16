925165b471a4ab2b43795ed86ff72c4fd88f8bed
chenxu89 <675269196@qq.com>
2016-01-16 14:07:43 +0800

    Initial Commit

---
+++ BabyHealth/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BabyHealth
+//
+//  Created by 陈旭 on 16/1/16.
+//  Copyright © 2016年 陈旭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
